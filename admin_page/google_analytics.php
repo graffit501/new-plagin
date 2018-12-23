336e733eaead965b8ac63ab193ad73ab874ef192
Andrey Mazur <pyatka.aag@gmail.com>
2018-12-23 16:29:33 +0000

    Fix prefixes, main file renaming and folder structure

---
+++ admin_page/google_analytics.php
@@ -0,0 +1,2 @@
+<h2>Первый плагин</h2>
+<p><?php echo "You can combine HTML and PHP to process your data"; ?></p>
---
+++ blog-plus.php
@@ -0,0 +1,21 @@
+<?php
+    /*
+    Plugin Name: Blog plus
+    Plugin URI: http://vk.com/graffit401
+    Description: Use only english for inner texts
+    Version:  1.0
+    Author: SerG
+    Author URI: http://vk.com/graffit401
+    License:      GPL2
+    License URI:  https://www.gnu.org/licenses/gpl-2.0.html
+    Text Domain:  blog-plus
+    */
+
+    add_action('admin_menu', function(){
+        add_options_page('Google Analytics', 'Google Analytics', 'manage_options', 'blog_plus_admin_menu_ga', function(){
+            ob_start();
+            include sprintf("admin_page/google_analytics.php");
+            echo ob_get_clean();
+        });
+    });
+?>
